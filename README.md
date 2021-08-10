603d8fe7ae7db555603f56916d6182e61010d023
Muhib Hasan <s3850034@student.rmit.edu.au>
2021-08-10 14:49:02 +1000

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+<h1>Project: Simon Says</h1>
+This project utilises JQuery to make an online Simon Says Game.
---
+++ game.js
@@ -0,0 +1,83 @@
+var gamePattern = []
+var userClickedPattern = []
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+//Main Code
+var gameStarted = false;
+var level = 0;
+
+$(document).keypress(function() {
+    if (!gameStarted) {
+        $("h1").text("Level " + level);
+        nextSequence();
+        gameStarted = true;
+    }
+})
+
+$(".btn").click(function() {
+    var userChosenColour = this.id;
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length - 1);
+});
+
+function nextSequence() {
+    userClickedPattern = [];
+    level++;
+    $("h1").text("Level " + level);
+    var min = Math.ceil(0);
+    var max = Math.floor(3);
+    var randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    
+    playSound(randomChosenColour);
+
+}
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+} 
+
+function animatePress(currentColour) {
+
+    $("#" + currentColour).addClass("pressed");
+
+    setTimeout(function() {
+        $("#" + currentColour).removeClass("pressed");
+    }, 100);
+}
+
+function checkAnswer(currentLevel) {
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+        console.log("success");
+        if(userClickedPattern.length === gamePattern.length) {
+            setTimeout(function() {
+                nextSequence();
+            }, 1000);
+        }
+    } else {
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+        }, 200);
+        $("h1").text("Game Over, Press Any Key to Restart");
+        startOver();
+        console.log("wrong");
+    }
+}
+
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    gameStarted = false;
+}
+
+
+
